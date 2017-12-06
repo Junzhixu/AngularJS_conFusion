698dda19e95a3102a17d43f4383ac407d0a851c8
Junzhixu <junzyxu@gmail.com>
2017-12-05 19:18:33 -0800

    Angular Components

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Dish } from '../shared/dish';
+import { DishdetailComponent } from '../dishdetail/dishdetail.component';
+
 
 const DISHES: Dish[] = [
                      {
